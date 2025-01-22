0b2c2359dd1171f554bd535e0b39e2346814958a
Vera Ama Pomaa <verapomaa99@gmail.com>
2025-01-22 02:05:07 +0000

    Added js-dropdown 1

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ let iconOpen = document.querySelector('.js-icon-open')
 let iconClose = document.querySelector('.js-icon-close')
 let options = document.querySelector('.js-options-mobile')
 let isOpened = false
+let isHovered = false
+let displayDropdown = document.querySelector('.js-dropdowns')
+let isArrowDown = true
+let  arrowDown = document.querySelector('.js-arrow-down')
+let  arrowUp = document.querySelector('.js-arrow-up')
+let featuresLink = document.querySelector('.js-features')
+let companyLink = document.querySelector('.js-company')
 
 iconOpen.addEventListener('click', () => {
  if(!isOpened){
@@ -17,6 +24,20 @@ iconOpen.addEventListener('click', () => {
 
    }
  })
+})
 
- 
+featuresLink.addEventListener('mouseover', () => {
+if(!isHovered){
+  arrowDown.classList.add('arrows-down')
+  arrowUp.classList.add('arrows-up')
+  isArrowDown = true
+  console.log('hovered')
+  isHovered = true
+}
+arrowDown.classList.remove('arrows-down')
+arrowUp.classList.remove('arrows-up')
+
+isArrowDown = false
+// console.log('is not hovered')
+// isHovered = false
 })
