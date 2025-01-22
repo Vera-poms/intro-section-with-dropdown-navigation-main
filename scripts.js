b3ac6c3d527488e90e6ddaaab9d8c8f379ec0061
Vera Ama Pomaa <verapomaa99@gmail.com>
2025-01-22 23:01:45 +0000

    Added dropdown 2

--- scripts.js
+++ scripts.js
@@ -3,10 +3,12 @@ let iconClose = document.querySelector('.js-icon-close')
 let options = document.querySelector('.js-options-mobile')
 let isOpened = false
 let isHovered = false
-let displayDropdown = document.querySelector('.js-dropdowns')
-let isArrowDown = true
-let  arrowDown = document.querySelector('.js-arrow-down')
-let  arrowUp = document.querySelector('.js-arrow-up')
+let displayFeaturesDropdown = document.querySelector('.js-features-dropdowns')
+let displayCompanyDropdown = document.querySelector('.js-company-dropdowns')
+let  arrowDown = document.querySelector('.js-features-arrow-down')
+let  companyArrowDown = document.querySelector('.js-company-arrow-down')
+let  arrowUp = document.querySelector('.js-features-arrow-up')
+let  companyArrowUp = document.querySelector('.js-company-arrow-up')
 let featuresLink = document.querySelector('.js-features')
 let companyLink = document.querySelector('.js-company')
 
@@ -21,23 +23,35 @@ iconOpen.addEventListener('click', () => {
     options.classList.remove('options-opened')
     console.log('closed')
     isOpened = false
-
    }
  })
 })
 
 featuresLink.addEventListener('mouseover', () => {
-if(!isHovered){
-  arrowDown.classList.add('arrows-down')
-  arrowUp.classList.add('arrows-up')
-  isArrowDown = true
-  console.log('hovered')
-  isHovered = true
-}
-arrowDown.classList.remove('arrows-down')
-arrowUp.classList.remove('arrows-up')
-
-isArrowDown = false
-// console.log('is not hovered')
-// isHovered = false
+  if(!isHovered){
+    isHovered = true
+    arrowDown.classList.add('arrows-down')
+    arrowUp.classList.add('arrows-up')
+    displayFeaturesDropdown.classList.add('dropdown-displayed')
+    console.log('hovered')
+  }else{
+    isHovered = false
+    arrowUp.classList.remove('arrows-up')
+    arrowDown.classList.remove('arrows-down')
+    displayFeaturesDropdown.classList.remove('dropdown-displayed')
+  } 
+})
+companyLink.addEventListener('mouseover', () => {
+  if(!isHovered){
+    isHovered = true
+    companyArrowDown.classList.add('arrows-down')
+    companyArrowUp.classList.add('arrows-up')
+    displayCompanyDropdown.classList.add('dropdown-displayed')
+    console.log('hovered')
+  }else{
+    isHovered = false
+    companyArrowUp.classList.remove('arrows-up')
+    companyArrowDown.classList.remove('arrows-down')
+    displayCompanyDropdown.classList.remove('dropdown-displayed')
+  } 
 })
