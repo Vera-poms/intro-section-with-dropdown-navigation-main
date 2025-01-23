fe73499e64a4b5083e15f73157a4416d67605cde
Vera Ama Pomaa <verapomaa99@gmail.com>
2025-01-23 20:42:08 +0000

    Added features and company options in mobile view

--- scripts.js
+++ scripts.js
@@ -1,39 +1,94 @@
 let iconOpen = document.querySelector('.js-icon-open')
 let iconClose = document.querySelector('.js-icon-close')
+let bodyOpacity = document.querySelector('.js-body-self')
 let options = document.querySelector('.js-options-mobile')
 let isOpened = false
+
+
+let isClicked = false
+let featuresLinkMobile = document.querySelector('.js-features-link-mobile')
+let featuresArrowDownMobile = document.querySelector('.js-features-arrow-down-mobile')
+let featuresArrowUpMobile = document.querySelector('.js-features-arrow-up-mobile')
+let featuresOptions = document.querySelector('.js-features-options')
+
+let companyLinkMobile = document.querySelector('.js-company-link-mobile')
+let companyArrowDownMobile = document.querySelector('.js-company-arrow-down-mobile')
+let companyArrowUpMobile = document.querySelector('.js-company-arrow-up-mobile')
+let companyOptions = document.querySelector('.js-company-options')
+
+
 let isHovered = false
 let displayFeaturesDropdown = document.querySelector('.js-features-dropdowns')
-let displayCompanyDropdown = document.querySelector('.js-company-dropdowns')
 let  arrowDown = document.querySelector('.js-features-arrow-down')
-let  companyArrowDown = document.querySelector('.js-company-arrow-down')
 let  arrowUp = document.querySelector('.js-features-arrow-up')
-let  companyArrowUp = document.querySelector('.js-company-arrow-up')
 let featuresLink = document.querySelector('.js-features')
+
+
+let displayCompanyDropdown = document.querySelector('.js-company-dropdowns')
+let  companyArrowDown = document.querySelector('.js-company-arrow-down')
+let  companyArrowUp = document.querySelector('.js-company-arrow-up')
 let companyLink = document.querySelector('.js-company')
 
 iconOpen.addEventListener('click', () => {
  if(!isOpened){
   options.classList.add('options-opened')
-  console.log('opened')
+  iconClose.classList.add('closed-mobile')
+  bodyOpacity.classList.add('opacity-added')
   isOpened = true
+ }else{
+  options.classList.remove('options-opened')
+  iconClose.classList.remove('closed-mobile')
+  bodyOpacity.classList.remove('opacity-added')
+  isOpened = false
  }
- iconClose.addEventListener('click', () => {
+ 
+})
+iconClose.addEventListener('click', () => {
   if(isOpened){
     options.classList.remove('options-opened')
-    console.log('closed')
+    iconClose.classList.remove('closed-mobile')
+    bodyOpacity.classList.remove('opacity-added')
     isOpened = false
+   }else{
+    isOpened = true
    }
  })
+
+featuresLinkMobile.addEventListener('click', () => {
+  if(!isClicked){
+    isClicked = true
+    featuresArrowDownMobile.classList.add('arrow-down-mobile')
+    featuresArrowUpMobile.classList.add('arrow-up-mobile')
+    featuresOptions.classList.add('features-options-displayed')
+    console.log('clicked')
+  }else{
+    isClicked = false
+    featuresArrowDownMobile.classList.remove('arrow-down-mobile')
+    featuresArrowUpMobile.classList.remove('arrow-up-mobile')
+    featuresOptions.classList.remove('features-options-displayed')
+  }
+})
+companyLinkMobile.addEventListener('click', () => {
+  if(!isClicked){
+    isClicked = true
+    companyArrowDownMobile.classList.add('company-arrows-down-mobile')
+    companyArrowUpMobile.classList.add('company-arrows-up-mobile')
+    companyOptions.classList.add('company-options-displayed')
+    console.log('clicked')
+  }else{
+    isClicked = false
+    companyArrowDownMobile.classList.remove('company-arrows-down-mobile')
+    companyArrowUpMobile.classList.remove('company-arrows-up-mobile')
+    companyOptions.classList.remove('company-options-displayed')
+  }
 })
 
-featuresLink.addEventListener('mouseover', () => {
+featuresLink.addEventListener('click', () => {
   if(!isHovered){
     isHovered = true
     arrowDown.classList.add('arrows-down')
     arrowUp.classList.add('arrows-up')
     displayFeaturesDropdown.classList.add('dropdown-displayed')
-    console.log('hovered')
   }else{
     isHovered = false
     arrowUp.classList.remove('arrows-up')
@@ -41,13 +96,12 @@ featuresLink.addEventListener('mouseover', () => {
     displayFeaturesDropdown.classList.remove('dropdown-displayed')
   } 
 })
-companyLink.addEventListener('mouseover', () => {
+companyLink.addEventListener('click', () => {
   if(!isHovered){
     isHovered = true
     companyArrowDown.classList.add('arrows-down')
     companyArrowUp.classList.add('arrows-up')
     displayCompanyDropdown.classList.add('dropdown-displayed')
-    console.log('hovered')
   }else{
     isHovered = false
     companyArrowUp.classList.remove('arrows-up')
